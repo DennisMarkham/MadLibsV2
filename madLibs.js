8ebea93ffe2ca2a7e5213585c527c50ad17c33ea
unknown <workdennis@yahoo.com>
2019-09-17 19:10:42 -0500

    lkjlk

--- madLibs.js
+++ madLibs.js
@@ -6,17 +6,23 @@ function checkInput()
   var fields = document.querySelectorAll("span");
   var blanks = 0;
 
- for (x = 1; x < fields.length; x++) { 
+ for (x = 1; x < fields.length + 1; x++) { 
  var wordType = $("#" + x + "i").parent().text();
+ var input = $("#" + x + "i").val();
       console.log(wordType);
-  if (wordType == "FAMOUS PERSON: " && $("#" + x + "i").val().match("^[A-Z]") == false)
+      console.log(input);
+  if (wordType == "FAMOUS PERSON: " && /^[A-Z]/.test(input) == false)
     //switching the && to an || results in a strange problem.  It seems it adds to the
   //error array because it does not go to the appropriate page, but no alert shows up.
   //but I did try to fix the regEx algorithim by taking from "regEx Tomfollery"
   //yet the check still does not work, even when I capitalize it and switch all the letters
-  
+  //saved older version on git.  Now.
 
-
+  //for whatever its worth, the last word type and input are never logged.  Could that be the
+  //reason for this error?
+  // adding "+1" made the blank check functional, but now never seems to past the 
+  //test and do "transMadLibsGather()"
+  //wait no, the rest does work now.  It just never shows the error.  Hmh
       {
         errors.push("Names must start with capital letter");
       }
@@ -35,7 +41,13 @@ function checkInput()
     }
 
     //regex check does not work, everything else runs smoothly, however.
-    if (errors.length < 0)
+    //alright, this was all so bizarre.  Remember you flipped the sign.
+    //that means before...OH!  Either way it goes down to the else-if 
+    //statement!  That explains it.
+
+    //okay, now I'm uploading this to git, and after this we are going to 
+    //make "blanks" just another error
+    if (errors.length > 0)
       {
         alert(errors.join('\n'));
       }
